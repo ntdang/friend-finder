5f3139cc948d80bc36a41975cf51955505a12c24
ntdang <ndang83@gmail.com>
2018-04-24 20:05:20 -0400

    Added more friends to API

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,114 @@
 module.exports = [
   {
     name: 'Nhi',
-    photo: '555.5555',
-    scores: [4, 4, 1, 3, 4, 1, 5, 5, 5, 1]
+    photo: 'https://media.licdn.com/dms/image/C5603AQF7HpYFmCxymA/profile-displayphoto-shrink_200_200/0?e=1529798400&v=beta&t=vr3yA2w3G-s1YcuPwdZ41lQ8u5VKTieSiOVChlAjezM',
+    scores: [
+      4,
+      4,
+      1,
+      3,
+      4,
+      1,
+      5,
+      5,
+      5,
+      1
+    ]
+  },
+  {
+    name: 'Chandler',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Matthew_Perry_as_Chandler_Bing.jpg/250px-Matthew_Perry_as_Chandler_Bing.jpg',
+    scores: [
+      3,
+      2,
+      1,
+      4,
+      2,
+      3,
+      4,
+      3,
+      5,
+      2
+    ]
+  },
+  {
+    name: 'Ross',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/240px-David_Schwimmer_as_Ross_Geller.jpg',
+    scores: [
+      4,
+      4,
+      5,
+      4,
+      2,
+      1,
+      4,
+      3,
+      1,
+      3
+    ]
+  },
+  {
+    name: 'Joey',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg/240px-Matt_LeBlanc_as_Joey_Tribbiani.jpg',
+    scores: [
+      4,
+      5,
+      3,
+      1,
+      2,
+      3,
+      3,
+      3,
+      4,
+      2
+    ]
+  },
+  {
+    name: 'Monica',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Courteney_Cox_as_Monica_Geller.jpg/240px-Courteney_Cox_as_Monica_Geller.jpg',
+    scores: [
+      3,
+      4,
+      1,
+      3,
+      2,
+      1,
+      1,
+      3,
+      5,
+      1
+    ]
+  },
+  {
+    name: 'Phoebe',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/240px-Friendsphoebe.jpg',
+    scores: [
+      5,
+      5,
+      3,
+      1,
+      4,
+      4,
+      5,
+      5,
+      4,
+      2
+    ]
+  },
+  {
+    name: 'Rachel',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Rachel_Green_Rachel_haircut.jpg/170px-Rachel_Green_Rachel_haircut.jpg',
+    scores: [
+      2,
+      3,
+      3,
+      1,
+      2,
+      3,
+      2,
+      4,
+      3,
+      4
+    ]
   }
 ];
