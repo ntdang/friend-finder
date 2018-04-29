9d292476c772002b6e93f4b75e5f06091cb0f7fb
ntdang <ndang83@gmail.com>
2018-04-29 16:46:56 -0400

    Score diffs work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,13 @@
 var friends = require('../data/friends');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get ALL friends
-  app.get('/api/friends', function(req, res) {
+  app.get('/api/friends', function (req, res) {
     return res.json(friends);
   });
 
   // POST a new friend
-  app.post('/api/friends', function(req, res) {
+  app.post('/api/friends', function (req, res) {
     var newFriend = req.body;
     var newFriendName = req.body.name;
     var newFriendPhoto = req.body.photo;
@@ -26,16 +26,30 @@ module.exports = function(app) {
     for (var i = 0; i < friends.length; i++) {
       var compareScores = friends[i].scores;
       console.log('Comparison scores are: ' + compareScores);
-    }
-    //loops through and subtracts existing scores from new score
-    for (var i = 0; i < friends.length; i++) {
-      var totalDiff = 0;
-      for (var j = 0; j < 10; j++) {
-          let scoreDiff = Math.abs(friends[i].scores[j] - newFriend.scores[j]);
-          totalDiff += scoreDiff;
+      //add up each of the existing scores then put into an array
+      var compTotals = 0;
+      for (var j = 0; j < compareScores.length; j++) {
+        compTotals += parseInt(compareScores[j]);
+        var scoreDiff = Math.abs(compTotals - newScore);
       }
-      console.log('Total diff is: ' + totalDiff);
-    };
+      console.log('Comp totals are: ' + compTotals);
+      console.log('Score diffs are: ' + scoreDiff);
+    }
+
+    //loops through and subtracts existing scores from new score
+    // for (var i = 0; i < friends.length; i++) {
+    //   var totalDiff = 0;
+    //   var totalDiffArr = [];
+    //   for (var j = 0; j < 10; j++) {
+    //       var scoreDiff = Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+    //       totalDiff += scoreDiff;
+    //       // totalDiffArr.push(totalDiff);
+    //   }
+    //   console.log('Total diff is: ' + totalDiff);
+    //   // console.log('Array is: ' + totalDiffArr);
+    // };
+    //DO WE NEED TO MAKE AN ARRAY TO FIND THE MIN???
+
 
     //add newFriend to friends array
     friends.push(newFriend);
@@ -44,19 +58,3 @@ module.exports = function(app) {
     res.json(newFriend);
   });
 };
-
-  //compare new friend values to existing friends values
-      
-        //select closest match
-
-        //display modal with name and picture of closest match
-      // Determine the user's most compatible friend using the following as a guide:
-
-      // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-      // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-      // Example:
-      // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-      // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-      // Total Difference: 2 + 1 + 2 = 5
-      // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-      // The closest match will be the user with the least amount of difference.
